ed45bb729a31775d6437ae8bdb164c9b7fd14bb0
Jeffrey Kim <jeffreyk93@gmail.com>
2021-07-16 14:43:30 -0400

    Add ability to select conversation in sidebar

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useConversations } from '../contexts/ConversationsProvider'
+import './styles/Conversations.scss'
 
 export default function Conversations() {
-  const {conversations} = useConversations();
+  const {conversations, selectConversationIndex} = useConversations();
 
   return (
     <div>
       {conversations.map((conversation, index) => (
-        <li key={index}>
+        <li key={index} onClick={() => selectConversationIndex(index)} className={conversation.selected && "selected"}>
           {conversation.recipients.map(r => r.name).join(', ')}
         </li>
       ))}
